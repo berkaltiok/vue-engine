664a6b781d9d56189aa31db046ae8f9bd2a33c18
Berk Altıok <berkpw@gmail.com>
2020-03-19 12:19:06 +0300

    Add vue-meta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
-var app = new Vue({
+Vue.use(VueMeta);
+
+let app = new Vue({
   el: '#app',
-  router: router
+  router: router,
+  metaInfo() {
+    return {
+      "title": "pirdweb",
+      "titleTemplate": "%s - Pird",
+      "meta": [
+        {"charset": "utf-8"},
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+        {"name": "description", "content": "Pird Tech Web Site"}
+      ],
+      "style": [],
+      "script": []
+    }
+  }
 })
